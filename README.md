b10d0d44242de7d7023ce5366f7ae4d5e7ae3790
simran019 <77523198+simran019@users.noreply.github.com>
2021-06-22 05:09:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-37-part-1
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(1000,800);
+  
+}
+
+
+function draw(){
+  background("pink");
+  
+}
